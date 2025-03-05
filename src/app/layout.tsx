39a7fde86f0ee2f08e1aadcb0aa72bd8d8597e80
ALI <ali-dev@texastechdemo.com>
2025-03-06 03:30:09 +0500

    Added vercel speed insights [on main layout]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
